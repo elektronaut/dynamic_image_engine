480524bc535694348af703d30e272ffe8bdf00d8
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2007-02-15 10:39:13 +0000

    Removed author_id on image migrations
    git-svn-id: http://svn.manualdesign.no/svn/backstage/plugins/dynamic_image/trunk@554 a47e93f7-a817-0410-9d0b-c952cbe0a5d1

--- db/migrate/002_create_images.rb
+++ db/migrate/002_create_images.rb
@@ -3,7 +3,6 @@ class CreateImages < ActiveRecord::Migration
 		create_table :images do |t|
 			t.column :name,         :string
 			t.column :byline,       :string
-			t.column :user_id,      :integer
 			t.column :filename,     :string
 			t.column :content_type, :string
 			t.column :size,         :string
