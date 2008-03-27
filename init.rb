c66ff297091b5f16d23ad817add3c4e3f2bb1298
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2008-03-27 14:47:25 +0000

    dynamic_image: Fix for rails 2.0
    git-svn-id: http://svn.manualdesign.no/svn/backstage/plugins/dynamic_image/trunk@1578 a47e93f7-a817-0410-9d0b-c952cbe0a5d1

--- init.rb
+++ init.rb
@@ -4,3 +4,12 @@ require 'vector2d'
 
 # Include hook code here
 require 'dynamic_image'
+
+# Evil, evil hack to support reloading of models
+Rails::Plugin.class_eval do
+	def reloadable!
+		load_paths.each { |p| Dependencies.load_once_paths.delete(p) }
+	end
+end
+
+reloadable!
