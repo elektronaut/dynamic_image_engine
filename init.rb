ad4f4e6228dde993c79007cd8fadf229073d0043
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2008-09-12 08:58:14 +0000

    backstage, dynamic_image: Compatibility fix
    git-svn-id: http://svn.manualdesign.no/svn/backstage/plugins/dynamic_image/trunk@2024 a47e93f7-a817-0410-9d0b-c952cbe0a5d1

--- init.rb
+++ init.rb
@@ -6,18 +6,18 @@ require 'vector2d'
 require 'dynamic_image'
 
 # Evil, evil hack to support reloading of models
-begin
-	# Rails 2.0
-	Rails::Plugin.class_eval do
-    	def reloadable!
-    	    if ActiveSupport.const_defined?("Dependencies")
-        		load_paths.each { |p| ActiveSupport::Dependencies.load_once_paths.delete(p) }
-            else
-        		load_paths.each { |p| Dependencies.load_once_paths.delete(p) }
-            end
-    	end
-	end
-	reloadable!
-rescue
-	# Rails 1.x
-end
+#begin
+#	# Rails 2.0
+#	Rails::Plugin.class_eval do
+#    	def reloadable!
+#    	    if ActiveSupport.const_defined?("Dependencies")
+#        		load_paths.each { |p| ActiveSupport::Dependencies.load_once_paths.delete(p) }
+#            else
+#        		load_paths.each { |p| Dependencies.load_once_paths.delete(p) }
+#            end
+#    	end
+#	end
+#	reloadable!
+#rescue
+#	# Rails 1.x
+#end
