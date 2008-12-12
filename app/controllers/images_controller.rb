28cdfd28bd6ea46ec93385683f3256324850d3a3
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2008-12-12 11:02:28 +0000

    backstage, dynamic_image: Various optimizations
    git-svn-id: http://svn.manualdesign.no/svn/backstage/plugins/dynamic_image/trunk@2429 a47e93f7-a817-0410-9d0b-c952cbe0a5d1

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,24 +1,35 @@
 # Controller for the DynamicImage engine
 class ImagesController < ApplicationController
 	
-	# Return the requested image. Rescale, filter and cache it where appropriate.
-	def view_image
-		minTime = Time.rfc2822( request.env[ "HTTP_IF_MODIFIED_SINCE" ] ) rescue nil
+	session :off
 
-		image = Image.find( params[:id] ) rescue nil
-		unless image
-			if self.respond_to?( :render_error )
-				render_error 404 and return
-			else
-				render :status => 404, :text => "404: Image not found" and return
-			end
+	def render_missing_image
+		if self.respond_to?( :render_error )
+			render_error 404
+		else
+			render :status => 404, :text => "404: Image not found"
 		end
+	end
+	protected :render_missing_image
+
+	# Return the requested image. Rescale, filter and cache it where appropriate.
+	def render_dynamic_image
+		minTime = Time.rfc2822(request.env["HTTP_IF_MODIFIED_SINCE"]) rescue nil
+
+		render_missing_image and return unless Image.exists?(params[:id])
+
+		image = Image.find(params[:id])
 
 		if minTime && image.created_at? && image.created_at <= minTime
 			render :text => '304 Not Modified', :status => 304
 			return
 		end
 		
+		unless image.data
+			logger.warn "Image #{image.id} exists, but has no data"
+			render_missing_image and return
+		end
+
 		if size = params[:size]
 		    if size =~ /^x[\d]+$/ || size =~ /^[\d]+x$/
 		        size = Vector2d.new(size)
